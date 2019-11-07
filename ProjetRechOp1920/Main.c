bc2fd61cb06f8598c36644548476b8d5d0c6070f
LotuxPunk <clement@vandendaelen.com>
2019-11-07 12:27:35 +0100

    First part

--- ProjetRechOp1920/Main.c
+++ ProjetRechOp1920/Main.c
@@ -1,6 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-void main(void) {
+bool isHullDobell(int a, int c, int m, int x0);
+int pgcd(int, int);
 
+void main(void)
+{
+	int a, c, m, x0;
+	char* buffer;
+
+	printf("Encodez le a : ");
+	fgets(buffer, INT_MAX, stdin);
+	a = atoi(buffer);
+
+	printf("Encodez le c : ");
+	fgets(buffer, INT_MAX, stdin);
+	c = atoi(buffer);
+
+	printf("Encodez le m : ");
+	fgets(buffer, INT_MAX, stdin);
+	m = atoi(buffer);
+
+	printf("Encodez le x0 : ");
+	fgets(buffer, INT_MAX, stdin);
+	x0 = atoi(buffer);
+}
+
+bool isHullDobell(int a, int c, int m, int x0)
+{
+	bool cond1 = pgcd(c, m) == 1;
+}
+
+int pgcd(int a, int b)
+{
+	int r;
+	while (b != 0)
+	{
+		r = a % b;
+		a = b;
+		b = r;
+	}
+	return a;
 }
---
+++ ProjetRechOp1920/Main.h
@@ -0,0 +1,3 @@
+#pragma once
+
+bool isHullDobell(int a, int c, int m, int x0);
