33a76089d172b3e10a6e579794ce2ec9a41e3bae
LotuxPunk <clement@vandendaelen.com>
2019-11-21 12:21:08 +0100

    cond2

--- ProjetRechOp1920/Main.c
+++ ProjetRechOp1920/Main.c
@@ -5,13 +5,11 @@
 #define INITIAL_CAPACITY 2
 
 bool isHullDobell(int a, int c, int m, int x0);
-void decompose(int n, int* facteursPremier);
 int pgcd(int, int);
-void push(int* arr, int index, int value, int* size, int* capacity);
 
 void main(void)
 {
-	/*int a, c, m, x0;
+	int a, c, m, x0;
 	char* buffer;
 
 	printf("Encodez le a : ");
@@ -28,16 +26,24 @@ void main(void)
 
 	printf("Encodez le x0 : ");
 	fgets(buffer, INT_MAX, stdin);
-	x0 = atoi(buffer);*/
-
-	int* arr = malloc(INITIAL_CAPACITY * sizeof(int));
-
-	decompose(55, &arr);
-	system("pause");
+	x0 = atoi(buffer);
 }
 
 bool isHullDobell(int a, int c, int m, int x0)
 {
 	bool cond1 = pgcd(c, m) == 1;
 	bool cond2 = (a - 1) % (c - 1) == 0;
+
+}
+
+int pgcd(int a, int b)
+{
+	int r;
+	while (b != 0)
+	{
+		r = a % b;
+		a = b;
+		b = r;
+	}
+	return a;
 }
